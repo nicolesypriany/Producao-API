2ebce5303b4fe23341a2156949d8e337cd29605d
Nicole Sypriany <nicolesypriany@gmail.com>
2024-12-07 11:58:51 -0300

    first commit

---
+++ ProducaoAPI/ProducaoAPI/Controllers/MaquinaController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ProducaoAPI.Data;
+using ProducaoAPI.Models;
+using ProducaoAPI.Requests;
+using ProducaoAPI.Responses;
+
+namespace ProducaoAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MaquinaController : ControllerBase
+    {
+        private readonly ProducaoContext _context;
+
+        public MaquinaController(ProducaoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Maquina>>> ListarMaquinas()
+        {
+            var maquinas = await _context.Maquinas.Where(m => m.Ativo == true).ToListAsync();
+            if (maquinas == null) return NotFound();
+            var maquinasResponse = EntityListToResponseList(maquinas);
+            return Ok(maquinasResponse);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Maquina>> BuscarMaquinaPorId(int id)
+        {
+            var maquina = await _context.Maquinas.FindAsync(id);
+            if(maquina == null) return NotFound();
+            return Ok(maquina);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Maquina>> CadastrarMaquina(MaquinaRequest req)
+        {
+            var maquina = new Maquina(req.Nome, req.Marca);
+            await _context.Maquinas.AddAsync(maquina);
+            await _context.SaveChangesAsync();
+            return Ok(maquina);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Maquina>> AtualizarMaquina(int id, MaquinaRequest req)
+        {
+            var maquina = await _context.Maquinas.FindAsync(id);
+            if (maquina == null) return NotFound();
+
+            maquina.Nome = req.Nome;
+            maquina.Marca = req.Marca;
+
+            await _context.SaveChangesAsync();
+            return Ok(maquina);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<Maquina>> InativarMaquina(int id)
+        {
+            var maquina = await _context.Maquinas.FindAsync(id);
+            if (maquina == null) return NotFound();
+            maquina.Ativo = false;
+
+            await _context.SaveChangesAsync();
+            return Ok(maquina);
+        }
+
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<Maquina>> DeletarMaquina(int id)
+        //{
+        //    var maquina = await _context.Maquinas.FindAsync(id);
+        //    if (maquina == null) return NotFound();
+
+        //     _context.Maquinas.Remove(maquina);
+        //    await _context.SaveChangesAsync();
+        //    return NoContent();
+        //}
+        
+        private static ICollection<MaquinaResponse> EntityListToResponseList(IEnumerable<Maquina> maquinas)
+        {
+            return maquinas.Select(m => EntityToResponse(m)).ToList();
+        }
+
+        private static MaquinaResponse EntityToResponse(Maquina maquina)
+        {
+            return new MaquinaResponse(maquina.Id, maquina.Nome, maquina.Marca);
+        }
+    }
+}
---
+++ ProducaoAPI/ProducaoAPI/Models/Forma.cs
@@ -0,0 +1,22 @@
+﻿namespace ProducaoAPI.Models
+{
+    public class Forma
+    {
+        public Forma(string nome, int produtoId, int pecasPorCiclo)
+        {
+            Nome = nome;
+            ProdutoId = produtoId;
+            PecasPorCiclo = pecasPorCiclo;
+            Ativo = true;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Produto Produto { get; set; }
+        public int ProdutoId { get; set; }
+        public int PecasPorCiclo { get; set; }
+        public ICollection<Maquina> Maquinas { get; set; }
+        public ICollection<ProcessoProducao> Producoes { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
