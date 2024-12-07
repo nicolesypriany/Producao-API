c5dfc264ad451e0af0bf7ef8b08475b698346b9e
Nicole Sypriany <nicolesypriany@gmail.com>
2024-12-07 12:39:46 -0300

    changed connection string db to appsettings

--- ProducaoAPI/ProducaoAPI/Program.cs
+++ ProducaoAPI/ProducaoAPI/Program.cs
@@ -10,8 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ProducaoContext>();
-//builder.Services.AddDbContext<ProducaoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// builder.Services.AddDbContext<ProducaoContext>();
+builder.Services.AddDbContext<ProducaoContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
