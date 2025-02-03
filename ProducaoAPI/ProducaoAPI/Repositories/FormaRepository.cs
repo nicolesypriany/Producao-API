a73eabdb14c0f76c60fa5a24a7eab6810a0093dc
Nicole Sypriany <nicolesypriany@gmail.com>
2025-02-02 21:27:15 -0300

    formas' first exceptions handling and tests applied

--- ProducaoAPI/ProducaoAPI/Repositories/FormaRepository.cs
+++ ProducaoAPI/ProducaoAPI/Repositories/FormaRepository.cs
@@ -15,18 +15,50 @@ namespace ProducaoAPI.Repositories
 
         public async Task<IEnumerable<Forma>> ListarFormasAsync()
         {
+            try
+            {
+                var formas = await _context.Formas.Where(m => m.Ativo == true).Include(f => f.Maquinas).Include(f => f.Produto).ToListAsync();
+                if (formas == null || formas.Count == 0) throw new NullReferenceException("Não !");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
             return await _context.Formas.Where(m => m.Ativo == true).Include(f => f.Maquinas).Include(f => f.Produto).ToListAsync();
         }
 
         public async Task<Forma> BuscarFormaPorIdAsync(int id)
         {
-            return await _context.Formas.Include(f => f.Maquinas).Include(f => f.Produto).FirstOrDefaultAsync(f => f.Id == id);
+            try
+            {
+                var forma = await _context.Formas.Include(f => f.Maquinas).Include(f => f.Produto).FirstOrDefaultAsync(f => f.Id == id);
+                if (forma == null) throw new NullReferenceException("ID da forma não encontrado!");
+                return forma;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public async Task AdicionarAsync(Forma forma)
         {
-            await _context.Formas.AddAsync(forma);
-            await _context.SaveChangesAsync();
+            try
+            {
+                if (string.IsNullOrWhiteSpace(forma.Nome)) throw new ArgumentException("O campo \"Nome\" não pode estar vazio.");
+
+                if (forma.PecasPorCiclo < 1) throw new ArgumentException("O número de peças por ciclo deve ser maior do que 0.");
+
+                //OBS.: Alterar coluna de peças por ciclo no banco de dados para adicionar regra de que o número deve ser maior que 0.
+
+                await _context.Formas.AddAsync(forma);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public async Task AtualizarAsync(Forma forma)
